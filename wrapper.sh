59f433631c32cbf6c5d6c2038fc5682313a4900b
mpochron <7736131+mpochron@users.noreply.github.com>
2018-12-23 22:15:16 +0100

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -16,16 +16,16 @@ fi
 sed -i -e '/web:/!b' -e ':a' -e "s/  host.*/  host: $HOST_NAME/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
 ## Set MySQL/RabbitMQ usernames/passwords
 ### MySQL Main DB
-sed -i -e '/main_db:/!b' -e ':a' -e "s/host.*/host: mysql/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
-sed -i -e'/main_db:/!b' -e ':a' -e "s/username.*/username: root/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
-sed -i -e'/main_db:/!b' -e ':a' -e "s/password.*/password: $MYSQL_ROOT_PASSWORD/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
+sed -i -e '/main_db:/!b' -e ':a' -e "s/host.*/host: $MYSQL_HOST/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
+sed -i -e'/main_db:/!b' -e ':a' -e "s/username.*/username: $MYSQL_USERNAME/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
+sed -i -e'/main_db:/!b' -e ':a' -e "s/password.*/password: $MYSQL_PASSWORD/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
 sed -i -e'/main_db:/!b' -e ':a' -e "s/  database.*/  database: $MYSQL_DATABASE/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
 ### MySQL Message DB
-sed -i -e '/message_db:/!b' -e ':a' -e "s/host.*/host: mysql/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
-sed -i -e'/message_db:/!b' -e ':a' -e "s/username.*/username: root/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
-sed -i -e'/message_db:/!b' -e ':a' -e "s/password.*/password: $MYSQL_ROOT_PASSWORD/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
+sed -i -e '/message_db:/!b' -e ':a' -e "s/host.*/host: $MYSQL_HOST/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
+sed -i -e'/message_db:/!b' -e ':a' -e "s/username.*/username: $MYSQL_USERNAME/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
+sed -i -e'/message_db:/!b' -e ':a' -e "s/password.*/password: $MYSQL_PASSWORD/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
 ### RabbitMQ
-sed -i -e '/rabbitmq:/!b' -e ':a' -e "s/host.*/host: rabbitmq/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
+sed -i -e '/rabbitmq:/!b' -e ':a' -e "s/host.*/host: $RABBITMQ_HOST/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
 sed -i -e '/rabbitmq:/!b' -e ':a' -e "s/username.*/username: $RABBITMQ_DEFAULT_USER/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
 sed -i -e '/rabbitmq:/!b' -e ':a' -e "s/password.*/password: $RABBITMQ_DEFAULT_PASS/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
 sed -i -e '/rabbitmq:/!b' -e ':a' -e "s/vhost.*/vhost: \/$RABBITMQ_DEFAULT_VHOST/;t trail" -e 'n;ba' -e ':trail' -e 'n;btrail' /opt/postal/config/postal.yml
@@ -42,7 +42,7 @@ rm -rf /opt/postal/tmp/pids/*
 
 ## Wait for MySQL to start up
 echo "== Waiting for MySQL to start up =="
-while ! mysqladmin ping -h mysql --silent; do
+while ! mysqladmin ping -h $MYSQL_HOST --silent; do
     sleep 0.5
 done
 
